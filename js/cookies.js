bf00596d2de2498c784d56f5e503acbf8b235edf
jacksmithinsulander <666.jack.smith@protonmail.com>
2022-10-06 21:10:17 +0200

    synced

---
+++ js/cookies.js
@@ -0,0 +1,34 @@
+let currentTheme = '';
+
+const toggleThemeBtn = document.querySelector('#checkbox');
+toggleThemeBtn.addEventListener('click', toggleTheme);
+
+const cookieExists = (document.cookie.match(/^(?:.*;)?\s*themeColor\s*=\s*([^;]+)(?:.*)?$/)||[,null])[1];
+
+if (cookieExists !== null) {
+	currentTheme = cookieExists;
+	} else if (window.matchMedia) {
+	currentTheme = window.matchMedia('(prefers-color-scheme: dark)') ? 'dark' : 'light';
+}
+updateDarkThemeCssClass();
+
+function toggleTheme() {
+	if (currentTheme === 'dark') {
+		document.cookie = "themeColor=light;path=/;domain=../subdomains/aboutMe.html";
+		currentTheme = 'light';
+	} else {
+		document.cookie = "themeColor=dark;path=/;domain=../subdomains/contactMe.html";
+		currentTheme = 'dark';
+	}
+	console.log('toggle theme to', currentTheme);
+	updateDarkThemeCssClass();
+}
+
+function updateDarkThemeCssClass() {
+	console.log('switch css class');
+	if (currentTheme === 'dark') {
+		document.body.classList.add('dark-theme');
+	} else {
+		document.body.classList.remove('dark-theme');
+	}
+}
